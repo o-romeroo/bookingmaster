f7c54a49271091c135b380f64b5dbae74ceb472d
gramos22 <gabrielrmzf@gmail.com>
2023-12-24 14:09:04 -0300

    🚀: initial commit

---
+++ src/test/java/com/gramos22/bookingmaster/BookingmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gramos22.bookingmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
