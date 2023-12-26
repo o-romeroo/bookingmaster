5f6bf6bd1a4177b843ae7fba84b18a788fac3511
gramos22 <gabrielrmzf@gmail.com>
2023-12-26 20:06:17 -0300

    feat: add UserController

--- src/main/java/com/gramos22/bookingmaster/controllers/UserController.java
+++ src/main/java/com/gramos22/bookingmaster/controllers/UserController.java
@@ -1,4 +1,22 @@
 package com.gramos22.bookingmaster.controllers;
 
+import com.gramos22.bookingmaster.dtos.UserRequest;
+import com.gramos22.bookingmaster.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("/users")
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping // create
+    public void addUser(@RequestBody UserRequest userRequest) {userService.addUser(userRequest);}
+
+    @GetMapping("/{id}") // read
+    public UserResponse getUserById(int id) {return userService.getUserById(id);}
+
+    @DeleteMapping("/{id}")
+    public UserResponse deleteUserById(int id) {return userService.deleteUserById(id);} 
+
 }
